7d4b5570660858a25479316e4d320dc95ed48d33
Radosław <teammenpl@gmail.com>
2023-02-21 14:52:55 +0100

    Small fix

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const text = document.querySelector(".sec-text");
 
-const textLoad = () =>{
-    setTimeout(() =>{
-        text.textContent = "Front-End";
-    }, 0);
-    setTimeout(() =>{
-        text.textContent = "Student";
-    }, 4000);
-    setTimeout(() =>{
-        text.textContent = "Unity Dev";
-    }, 8000);
-}
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = "Back-End";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Student";
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "Unity Dev";
+  }, 8000);
+};
 
-    textLoad();
-    setInterval(textLoad, 12000);
+textLoad();
+setInterval(textLoad, 12000);
